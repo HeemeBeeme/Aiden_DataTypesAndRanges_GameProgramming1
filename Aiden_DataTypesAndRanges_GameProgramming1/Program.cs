8b98b36eb3d08864b9d316b48a2f61ab63dfca4f
Heeme Beeme <heemebeeme@gmail.com>
2025-09-23 16:19:55 -0300

    int, uint, short, ushort examples

--- Aiden_DataTypesAndRanges_GameProgramming1/Program.cs
+++ Aiden_DataTypesAndRanges_GameProgramming1/Program.cs
@@ -8,8 +8,39 @@ namespace Aiden_DataTypesAndRanges_GameProgramming1
 {
     internal class Program
     {
+        static int intExample = -2147483648;
+        static uint uintExample = 0;
+        static short shortExample = -32768;
+        static ushort ushortExample = 0;
+
+        static void IntExampleText()
+        {
+            Console.WriteLine($"An int is an integer, hence the name. The smallest number you can have with an int is {intExample}\nand the biggest number is {intExample = 2147483647}");
+            Console.ReadKey();
+            Console.WriteLine($"The smallest number you can have with a uint is: {uintExample}\nThe biggest number you can have with a uint is: {uintExample = 4294967295}");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine($"uint stands for unsigned int, which means the value cannot be negative.");
+            Console.ReadKey();
+            Console.WriteLine("This also means that the maximum of uint is double the maximum value of int (plus one because the negative number inside of signed variables is one more than the positive numbers)");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void ShortExampleText()
+        {
+            Console.WriteLine($"The smallest number you can have with a short is: {shortExample}\nThe biggest number you can have with a uint is: {shortExample = 32767}");
+            Console.ReadKey();
+            Console.WriteLine($"A ushort has the same differences with a short as uint has with int, so smallest number you can have with a ushort is: {ushortExample}\nThe biggest number you can have with a ushort is: {ushortExample = 65535}");
+            Console.ReadKey();
+            Console.Clear();
+        }
         static void Main(string[] args)
         {
+            IntExampleText();
+            ShortExampleText();
+
+            
         }
     }
 }
